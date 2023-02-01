e016cd02479ca5fdbec6d030b6f90390e787aa1a
Lorena <lorenacapraru@gmail.com>
2023-02-01 18:53:05 +0000

    level 200

--- script.js
+++ script.js
@@ -1,48 +1,79 @@
 //You can edit ALL of the code here
-function makePageForEpisodes(episodeList) {
-  //const rootElem = document.getElementById("root");
-  //rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+const boxContainer = document.getElementById("boxContainer");
+const allEpisodes = getAllEpisodes();
+let searchContainer = document.getElementById("search");
+const resultsNo = document.createElement("p");
+
+function displaySearchResults(episodeList) {
+  resultsNo.innerHTML = `Got ${episodeList.length} episode(s)`;
+  searchContainer.appendChild(resultsNo);
 }
 
-const rootElem = document.getElementById("root");
-
 function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  displaySearchResults(allEpisodes);
   for (let episode of allEpisodes) {
-    let containerDiv = document.createElement("div");
-    rootElem.appendChild(containerDiv);
-    containerDiv.className = "box";
-
-    let seasonNo = document.createElement("span");
-    seasonNo.innerHTML = `S${Number(episode.season) > 10 ? 1 : 0}${
-      episode.season
-    }`;
-
-    let episodeNo = document.createElement("span");
-    episodeNo.innerText = `E${Number(episode.number) > 10 ? 1 : 0}${
-      episode.number
-    }`;
-
-    let title = document.createElement("h3");
-    title.innerHTML = `${episode.name.toUpperCase()} - `;
-    title.appendChild(seasonNo);
-    title.appendChild(episodeNo);
-    title.className = "title";
-
-    let episodeBrief = document.createElement("p");
-    episodeBrief.innerHTML = episode.summary;
-    episodeBrief.className = "brief";
-
-    let episodeImg = document.createElement("img");
-    episodeImg.src = episode.image.medium;
-    episodeImg.className = "image";
-    //episodeImg.alt = episode.alt;
-
-    containerDiv.appendChild(title);
-    containerDiv.appendChild(episodeImg);
-    containerDiv.appendChild(episodeBrief);
+    createBoxEpisode(episode);
   }
+  createSearch();
+}
+
+function createBoxEpisode(episode) {
+  let containerDiv = document.createElement("div");
+  boxContainer.appendChild(containerDiv);
+  containerDiv.className = "box";
+
+  let seasonNo = document.createElement("span");
+  seasonNo.innerHTML = `S${Number(episode.season) > 10 ? 1 : 0}${
+    episode.season
+  }`;
+
+  let episodeNo = document.createElement("span");
+  episodeNo.innerText = `E${Number(episode.number) > 10 ? 1 : 0}${
+    episode.number
+  }`;
+
+  let title = document.createElement("h3");
+  title.innerHTML = `${episode.name.toUpperCase()} - `;
+  title.appendChild(seasonNo);
+  title.appendChild(episodeNo);
+  title.className = "title";
+
+  let episodeBrief = document.createElement("p");
+  episodeBrief.innerHTML = episode.summary;
+  episodeBrief.className = "brief";
+
+  let episodeImg = document.createElement("img");
+  episodeImg.src = episode.image.medium;
+  episodeImg.className = "image";
+  //episodeImg.alt = episode.alt;
+
+  containerDiv.appendChild(title);
+  containerDiv.appendChild(episodeImg);
+  containerDiv.appendChild(episodeBrief);
+}
+
+function createSearch() {
+  let searchEpisodes = document.getElementById("searchEpisode");
+  let listOfSearchedEp = [];
+  searchEpisodes.addEventListener("input", () => {
+    boxContainer.innerHTML = "";
+    allEpisodes
+      .filter((episode) => {
+        return (
+          episode.name
+            .toLowerCase()
+            .includes(searchEpisodes.value.toLowerCase()) ||
+          episode.summary
+            .toLowerCase()
+            .includes(searchEpisodes.value.toLowerCase())
+        );
+      })
+      .forEach((ep) => {
+        createBoxEpisode(ep);
+        listOfSearchedEp.push(ep);
+      });
+    resultsNo.innerHTML = `Got ${boxContainer.childElementCount} episode(s)`;
+  });
 }
 
 window.onload = setup;
